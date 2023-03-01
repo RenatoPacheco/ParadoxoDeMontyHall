771bb8d005a4391d9ecb50f4e115608cec06fa06
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-03-01 06:30:32 -0300

    Renomeando variável

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ List<Cenario> cenarios = new()
     new Cenario(new EscolhaSempreDiferentePart(), "D"),
     new Cenario(new EscolhaSemRepetirAhAnteriorPart(), "E")
 };
-int quantidadeDeTentativas = 20000;
+int quantidadeDeJogos = 20000;
 int quantidadesDePortas = 5;
 List<Porta> portas;
 
-for (int i = 0; i < quantidadeDeTentativas; i++)
+for (int i = 0; i < quantidadeDeJogos; i++)
 {
     portas = Porta.PrepararPortas(quantidadesDePortas).ToList();
 
